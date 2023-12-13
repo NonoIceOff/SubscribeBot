216cc9617867c011246918ab031424038c2a7b8f
NTH3C <147485340+NTH3C@users.noreply.github.com>
2023-12-13 15:30:32 +0100

    subscribe + comment answer

--- main.py
+++ main.py
@@ -8,6 +8,24 @@ API_VERSION = "v3"
 SCOPES = ["https://www.googleapis.com/auth/youtube.force-ssl"]
 
 
+
+def reply_to_comment(youtube, video_id, parent_id, text ):
+    request = youtube.comments().insert(
+        part="snippet",
+        body={
+            "snippet": {
+                "parentId": parent_id,
+                "textOriginal": text
+            }
+        }
+    )
+    try:
+        response = request.execute()
+        print(f"Réponse au commentaire réussie : {text}")
+    except Exception as e:
+        print(f"Erreur lors de la réponse au commentaire : {str(e)}")
+
+
 def get_authenticated_service():
     flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
     credentials = flow.run_local_server(port=0)
@@ -47,24 +65,33 @@ def get_comment_threads(youtube, video_id, max_results):
     return results["items"]
 
 def subscribe_to_commenters(youtube, video_id, max_results=5):
-    commenters = get_comment_threads(youtube, video_id,max_results)
-    print("Récupération des commenteurs réussi !")
+    commenters = get_comment_threads(youtube, video_id, max_results)
+    print("Récupération des commentateurs réussie !")
 
-    for commenter_id in commenters:
-        id_guy = commenter_id["snippet"]["topLevelComment"]["snippet"]["authorChannelId"]["value"]
+    for commenter in commenters:
+        id_guy = commenter["snippet"]["topLevelComment"]["snippet"]["authorChannelId"]["value"]
+        comment_id = commenter["id"]
         print(f"ChaîneID {id_guy} récupérée")
-        if is_channel_subscribed(youtube) == False:
-            add_subscription(youtube, id_guy)
-            print(f"Abonnement à la chaîne {commenter_id['snippet']['authorDisplayName']} effectué.")
-            print("------------")
 
-def is_channel_subscribed(youtube):
+        if not is_channel_subscribed(youtube, id_guy):
+            add_subscription(youtube, id_guy)
+            print(f"Abonnement à la chaîne {id_guy}")
+
+            # Ajouter une réponse au commentaire
+            reply_text = "Je suis vraiment Apagnan de votre chaine youtube !"
+            reply_to_comment(youtube, video_id, comment_id, reply_text)
+
+
+def is_channel_subscribed(youtube, channelid_check):
     subscriptions_response = youtube.subscriptions().list(
         part='snippet',
         mine=True,
         maxResults=50,
         order='alphabetical').execute()
     print(subscriptions_response)
+    for item in subscriptions_response["items"]:
+        if channelid_check == item["snippet"]["resourceId"]["channelId"]:
+            return True
     return False
 
 if __name__ == '__main__':
